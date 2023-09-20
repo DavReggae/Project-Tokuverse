c4fb692b5a7dccfef5c3e6e189f81b4b3c9cee18
Thien Vong <thien.vong.892@my.csun.edu>
2023-09-20 11:38:06 -0700

    Small Change

--- app/src/main/java/com/example/tokuverseproject/MainActivity.java
+++ app/src/main/java/com/example/tokuverseproject/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.tokuverseproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView iconView;
+    Drawable drawable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        iconView.findViewById(R.id.iconView);
+        drawable = getResources().getDrawable(R.drawable.icon);
+
     }
 }
