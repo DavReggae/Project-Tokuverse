a830f9a22af97075a4b3a3ca6eef65c62382d3ec
Thien Vong <thien.vong.892@my.csun.edu>
2023-09-29 15:06:32 -0700

    Sign Up View basic

--- app/src/main/java/com/example/tokuverseproject/MainActivity.java
+++ app/src/main/java/com/example/tokuverseproject/MainActivity.java
@@ -2,21 +2,31 @@ package com.example.tokuverseproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
-    ImageView iconView;
-    Drawable drawable;
-
+    Button btn_GoToSignUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        iconView.findViewById(R.id.iconView);
-        drawable = getResources().getDrawable(R.drawable.icon);
-
+        btn_GoToSignUp = findViewById(R.id.btn_GoToSignUp);
+        btn_GoToSignUp.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view)
+            {
+                gotoSignUp();
+            }
+        });
+    }
+    void gotoSignUp()
+    {
+        Intent intent = new Intent(this, SignUp.class);
+        startActivity(intent);
     }
 }
