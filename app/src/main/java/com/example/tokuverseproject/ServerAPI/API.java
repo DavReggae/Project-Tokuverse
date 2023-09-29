b92c7aa1753f54c9f76167119b39b4a9071ef69e
Thien Vong <thien.vong.892@my.csun.edu>
2023-09-29 16:44:18 -0700

    Install RxJava3

---
+++ app/src/main/java/com/example/tokuverseproject/ServerAPI/API.java
@@ -0,0 +1,7 @@
+package com.example.tokuverseproject.ServerAPI;
+
+import io.reactivex.rxjava3.internal.operators.observable.ObservableSerialized;
+import retrofit2.http.GET;
+public interface API {
+
+}
--- app/src/main/java/com/example/tokuverseproject/ServerAPI/RetrofitClient.java
+++ app/src/main/java/com/example/tokuverseproject/ServerAPI/RetrofitClient.java
@@ -1,6 +1,8 @@
 package com.example.tokuverseproject.ServerAPI;
 
+import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
     private static Retrofit instance;
@@ -8,7 +10,11 @@ public class RetrofitClient {
     {
         if(instance == null)
         {
-            
+            instance = new Retrofit.Builder()
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+                    .build();
         }
+        return  instance;
     }
 }
