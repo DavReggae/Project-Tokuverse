794226e53e83f6fae035062d895009981cd73a8a
Thien Vong <124405875+OSkyLight@users.noreply.github.com>
2023-10-23 11:55:37 -0700

    Add files via upload

--- connect.php
+++ connect.php
@@ -3,13 +3,10 @@
     {
         $pdo = new PDO("mysql:host=sql3.freesqldatabase.com;dbname=sql3649857", "sql3649857", "Lplvvb2Ajh");
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "success";
+        echo "Connection Sucess";
     }
     catch(PDOException $e)
     {
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connection failed: " . $e->getMessage();
     }
-    
-    
-    
 ?>
