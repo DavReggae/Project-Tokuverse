47e4e10d7f0f1d25487292181ddd589372d5167a
Thien Vong <124405875+OSkyLight@users.noreply.github.com>
2023-11-01 12:12:30 -0700

    Add files via upload

---
+++ getHeroDetails_ByID.php
@@ -0,0 +1,15 @@
+<?php
+    include "connect.php";
+    $id = $_POST['id'];
+    $sql = "SELECT * FROM HERO_DETAILS WHERE id = '$id'";
+    try {
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+        $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $jsonData = json_encode($users);
+        echo $jsonData;
+    } 
+    catch (PDOException $e) {
+        die("Query failed: " . $e->getMessage());
+    }
+?>
