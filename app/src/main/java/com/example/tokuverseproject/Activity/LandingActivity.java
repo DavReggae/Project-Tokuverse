ef0f4d49673a5473cbf90a029934c472ca51ef64
David Regio <regio.david09@gmail.com>
2023-11-13 11:40:18 -0800

    test

--- app/src/main/java/com/example/tokuverseproject/Activity/LandingActivity.java
+++ app/src/main/java/com/example/tokuverseproject/Activity/LandingActivity.java
@@ -3,11 +3,13 @@ package com.example.tokuverseproject.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.tokuverseproject.R;
 
 public class LandingActivity extends AppCompatActivity {
 
+    Button btn_Test;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
