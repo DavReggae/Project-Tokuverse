7b8c8df404de6c9e0041378e6a5a0f7a992aa62f
David Regio <regio.david09@gmail.com>
2023-11-13 12:15:11 -0800

    Add Button Test

--- app/src/main/java/com/example/tokuverseproject/Activity/LandingActivity.java
+++ app/src/main/java/com/example/tokuverseproject/Activity/LandingActivity.java
@@ -2,7 +2,10 @@ package com.example.tokuverseproject.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 import com.example.tokuverseproject.R;
@@ -14,5 +17,22 @@ public class LandingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+        btn_Test = findViewById(R.id.btn_Test);
+        btn_Test.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view)
+            {
+                try {
+
+                }
+                catch(Exception e)
+                {
+                    Log.d("Tag", e.getMessage());
+                }
+                Log.d("Tag", " Tesst Message");
+                /* Intent intent = new Intent(LandingActivity.this, MainActivity.class);
+                startActivity(intent); */
+            }
+        });
     }
 }
