9890e864212d60b66cdc7ef8da5ee326e65b7d72
David Regio <regio.david09@gmail.com>
2023-12-03 21:14:51 -0800

    Changes made to Landing Page

--- app/src/main/java/com/example/tokuverseproject/Activity/LandingActivity.java
+++ app/src/main/java/com/example/tokuverseproject/Activity/LandingActivity.java
@@ -6,13 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.tokuverseproject.R;
 
 public class LandingActivity extends AppCompatActivity {
 
     Button btn_Test;
+    Button button3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +33,23 @@ public class LandingActivity extends AppCompatActivity {
                 {
                     Log.d("Tag", e.getMessage());
                 }
-                Log.d("Tag", " Tesst Message");
+                Log.d("Tag", " Test Message");
+                Intent intent = new Intent(LandingActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
+
+                }
+                catch(Exception e)
+                {
+                    Log.d("Tag", e.getMessage());
+                }
+                Log.d("Tag", " Test Message");
                 Intent intent = new Intent(LandingActivity.this, MainActivity.class);
                 startActivity(intent);
             }
