d2a26819ddaf848e26f064547a645c18f51d1346
AngelCSUN <angelgoku.rinos.678@my.csun.edu>
2024-05-01 12:26:15 -0700

    Update

--- script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", function() {
-    var signupLoginNav = document.getElementById("signup-login");
-    if (window.pageYOffset > 0) {
-      signupLoginNav.classList.add("hidden");
-    } else {
-      signupLoginNav.classList.remove("hidden");
-    }
-  });
